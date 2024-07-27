7c83ef2e91a3a90bc14fce12f15abdcf2c4632ec
Kamil Krzyśków <kamilzary@gmail.com>
2024-07-27 13:06:17 +0200

    feat: config api for unique_blog_date

--- mkdocs_nype/plugins/unique_blog_date/config.py
+++ mkdocs_nype/plugins/unique_blog_date/config.py
@@ -1,5 +1,8 @@
 from mkdocs.config import Config
+from mkdocs.config.config_options import Type
 
 
 class UniqueBlogDateConfig(Config):
-    pass
+
+    enabled = Type(bool, default=True)
+    hook_blog_dir = Type(str)
--- mkdocs_nype/plugins/unique_blog_date/plugin.py
+++ mkdocs_nype/plugins/unique_blog_date/plugin.py
@@ -23,51 +23,70 @@ from .config import UniqueBlogDateConfig
 
 # region Core Logic Events
 
-_ENV_REF = None
-_BLOG_FILTER_REF = None
-_CUSTOM_FILTER_REF = None
-
 
 class UniqueBlogDatePlugin(BasePlugin[UniqueBlogDateConfig]):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.env_ref = None
+        self.blog_filter_ref = None
+        self.custom_filter_ref = None
+        self._blog_root = None
+
+    def on_config(self, config: MkDocsConfig) -> MkDocsConfig | None:
+        """Validate a blog instance with the given root exists"""
+
+        blog_instance = None
+        for name, instance in config.plugins.items():
+            if name.split(" ")[0].endswith("/blog"):
+                if instance.config.blog_dir == self.config.hook_blog_dir:
+                    blog_instance = instance
+
+        if blog_instance is None:
+            LOG.warning(f"Can't find blog {self.config.hook_blog_dir}")
+            return
+
+        self._blog_root = self.config.hook_blog_dir.rstrip("/") + "/"
 
     # Run after the blog plugin
     @event_priority(-100)
     def on_env(self, env: Environment, *, config: MkDocsConfig, **__) -> Optional[Environment]:
         """Main function. Triggers just before the build begins."""
-        global _ENV_REF
-        global _BLOG_FILTER_REF
-        global _CUSTOM_FILTER_REF
 
-        _ENV_REF = env
+        if not self._blog_root:
+            return
 
-        if not _ENV_REF.filters.get("date"):
+        self.env_ref = env
+
+        if not self.env_ref.filters.get("date"):
             return
 
         def custom_date_filter(date):
             return _format_date(date, "yyyy MMMM", config)
 
-        _CUSTOM_FILTER_REF = custom_date_filter
-        _BLOG_FILTER_REF = env.filters["date"]
+        self.custom_filter_ref = custom_date_filter
+        self.blog_filter_ref = env.filters["date"]
 
         LOG.info("New blog date filter has been saved")
 
     @event_priority(-100)
     def on_page_context(self, context, page, config, nav):
-        """Depending on the page replace the filter"""
+        """Depending on the page url replace the filter"""
 
-        if not _ENV_REF.filters.get("date"):
+        if not self._blog_root or not self.env_ref.filters.get("date"):
             return
 
-        if page.url.startswith("exp/"):
+        if page.url.startswith(self._blog_root):
             LOG.debug("Custom filter applied")
-            _ENV_REF.filters["date"] = _CUSTOM_FILTER_REF
+            self.env_ref.filters["date"] = self.custom_filter_ref
         else:
             LOG.debug("Blog filter applied")
-            _ENV_REF.filters["date"] = _BLOG_FILTER_REF
+            self.env_ref.filters["date"] = self.blog_filter_ref
 
 
 def _format_date(date: datetime, format: str, config: MkDocsConfig):
-    """Copied from the blog plugin"""
+    """Copied from the material/blog plugin"""
     locale: str = config.theme["language"].replace("-", "_")
     if format in ["full", "long", "medium", "short"]:
         return format_date(date, format=format, locale=locale)
